bba8c6427dcfcfcd86f55364d4489554df583488
Hitesh Chouhan <hitesh.chouhan@cennest.com>
2014-11-15 19:00:43 +0530

    Added a singleton class

---
+++ WellnessManagementSystem/WPFUI/AppManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysioApplication
+{
+    public class AppManager
+    {
+        private static AppManager instance = null;
+        private int UserID;
+        private string UserName;
+
+        private AppManager()
+        {
+
+        }
+
+        public static AppManager getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new AppManager();
+            }
+            return instance;
+        }
+
+        public void SetUserName(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public string GetUserName()
+        {
+            return this.UserName;
+        }
+
+        public void setUserID(int userID)
+        {
+            this.UserID = userID;
+        }
+
+        public int GetUserID()
+        {
+            return this.UserID;
+        }
+    }
+}
