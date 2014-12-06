03ad8921973c56cced83d2570ab23e648b3c2cb2
vijay <git>
2014-12-06 17:09:16 +0530

    Created all client notification page.

--- WellnessManagementSystem/BusinessLayer/BusinessLayerManager.cs
+++ WellnessManagementSystem/BusinessLayer/BusinessLayerManager.cs
@@ -16,28 +16,38 @@ namespace BusinessLayer
             ClientSummary client1 = new ClientSummary();
             client1.ClientID = 1;
             client1.ClientName = "Annu Raj";
+            client1.ClientNotificationID = "No Notification";
+            client1.ClientLastNotes = "No Notes";
 
             ClientSummary client2 = new ClientSummary();
             client2.ClientID = 2;
             client2.ClientName = "Gagan Narang";
+            client2.ClientNotificationID = "No Notification";
+            client2.ClientLastNotes = "No Notes";
 
             ClientSummary client3 = new ClientSummary();
             client3.ClientID = 3;
             client3.ClientName = "Marry Kom";
+            client3.ClientNotificationID = "No Notification";
+            client3.ClientLastNotes = "No Notes";
 
             ClientSummary client4 = new ClientSummary();
             client4.ClientID = 4;
             client4.ClientName = "Pooja gatkar";
+            client4.ClientNotificationID = "No Notification";
+            client4.ClientLastNotes = "No Notes";
 
             ClientSummary client5 = new ClientSummary();
             client5.ClientID = 5;
             client5.ClientName = "Shiv Thapa";
+            client5.ClientNotificationID = "No Notification";
+            client5.ClientLastNotes = "No Notes";
 
             ClientSummary client6 = new ClientSummary();
             client6.ClientID = 6;
             client6.ClientName = "Shweta Choudhary";
 
-             List<ClientSummary> allClientSummary = new List<ClientSummary>();
+            List<ClientSummary> allClientSummary = new List<ClientSummary>();
             allClientSummary.Add(client1);
             allClientSummary.Add(client2);
             allClientSummary.Add(client3);
