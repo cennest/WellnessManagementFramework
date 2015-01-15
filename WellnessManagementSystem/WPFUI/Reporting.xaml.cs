993f525412b36e01afe14fc8def1c6c77fd79901
Hitesh Chouhan <hiteshchouhan91@hotmail.com>
2015-01-16 01:06:01 +0530

    Added line charting and some controls for report filtering

--- WellnessManagementSystem/WPFUI/Reporting.xaml.cs
+++ WellnessManagementSystem/WPFUI/Reporting.xaml.cs
@@ -28,23 +28,23 @@ namespace PhysioApplication
         public Reporting()
         {
             InitializeComponent();
-            BusinessLayerManager businessLayer = new BusinessLayerManager();
-            List<BOCategory> categoryList = businessLayer.GetAllCategories();
-            List<ComboBoxItem> comboBoxItemListForCategory = new List<ComboBoxItem>();
-            foreach (BOCategory category in categoryList)
-            {
-                comboBoxItemListForCategory.Add(new ComboBoxItem { Content = category.CategoryName, Tag = category.CategoryID.ToString() });
-            }
-            this.ComboBoxForCategory.ItemsSource = comboBoxItemListForCategory;
-            this.ComboBoxForCategory.SelectedIndex = 0;
-            List<BOReporting> reportList = businessLayer.GetAllReports();
-            List<ComboBoxItem> comboBoxItemListForReport = new List<ComboBoxItem>();
-            foreach (BOReporting report in reportList)
-            {
-                comboBoxItemListForReport.Add(new ComboBoxItem { Content = report.ReportFieldName, Tag = report.ReportFieldID.ToString() });
-            }
-            this.ComboBoxForReport.ItemsSource = comboBoxItemListForReport;
-            this.ComboBoxForReport.SelectedIndex = 0;
+            //BusinessLayerManager businessLayer = new BusinessLayerManager();
+            //List<BOCategory> categoryList = businessLayer.GetAllCategories();
+            //List<ComboBoxItem> comboBoxItemListForCategory = new List<ComboBoxItem>();
+            //foreach (BOCategory category in categoryList)
+            //{
+            //    comboBoxItemListForCategory.Add(new ComboBoxItem { Content = category.CategoryName, Tag = category.CategoryID.ToString() });
+            //}
+            //this.ComboBoxForCategory.ItemsSource = comboBoxItemListForCategory;
+            //this.ComboBoxForCategory.SelectedIndex = 0;
+            //List<BOReporting> reportList = businessLayer.GetAllReports();
+            //List<ComboBoxItem> comboBoxItemListForReport = new List<ComboBoxItem>();
+            //foreach (BOReporting report in reportList)
+            //{
+            //    comboBoxItemListForReport.Add(new ComboBoxItem { Content = report.ReportFieldName, Tag = report.ReportFieldID.ToString() });
+            //}
+            //this.ComboBoxForReport.ItemsSource = comboBoxItemListForReport;
+            //this.ComboBoxForReport.SelectedIndex = 0;
 
             LoadFirstSeries();
         }
@@ -52,21 +52,21 @@ namespace PhysioApplication
 
         private void LoadLineChartData()
         {
-            if (this.categoryID != 0 && this.reportID != 0)
-            {
-                BusinessLayerManager businessLayer = new BusinessLayerManager();
-                Hashtable listOfReportForCategoryHasTable = businessLayer.GetLabReportsForCategory(this.categoryID, 1, this.reportID);
-                foreach (DictionaryEntry entry in listOfReportForCategoryHasTable)
-                {
-                    Console.WriteLine("{0}, {1}", entry.Key, entry.Value);
-                    List<BOLabReport> labReportList = (List<BOLabReport>)listOfReportForCategoryHasTable[entry.Key];
-                    List<KeyValuePair<DateTime, int>> keyValuePair = new List<KeyValuePair<DateTime, int>>();
-                    foreach (BOLabReport labReport in labReportList)
-                    {
-                        keyValuePair.Add(new KeyValuePair<DateTime, int>(labReport.TestDate, Convert.ToInt32(labReport.ReportFieldValue)));
-                    }
-                    // ((LineSeries)mcChart.Series[0]).ItemsSource = keyValuePair;
-                }
+            //if (this.categoryID != 0 && this.reportID != 0)
+            //{
+            //    BusinessLayerManager businessLayer = new BusinessLayerManager();
+            //    Hashtable listOfReportForCategoryHasTable = businessLayer.GetLabReportsForCategory(this.categoryID, 1, this.reportID);
+            //    foreach (DictionaryEntry entry in listOfReportForCategoryHasTable)
+            //    {
+            //        Console.WriteLine("{0}, {1}", entry.Key, entry.Value);
+            //        List<BOLabReport> labReportList = (List<BOLabReport>)listOfReportForCategoryHasTable[entry.Key];
+            //        List<KeyValuePair<DateTime, int>> keyValuePair = new List<KeyValuePair<DateTime, int>>();
+            //        foreach (BOLabReport labReport in labReportList)
+            //        {
+            //            keyValuePair.Add(new KeyValuePair<DateTime, int>(labReport.TestDate, Convert.ToInt32(labReport.ReportFieldValue)));
+            //        }
+            //        // ((LineSeries)mcChart.Series[0]).ItemsSource = keyValuePair;
+            //    }
                 //((LineSeries)mcChart.Series[0]).ItemsSource =
                 // new KeyValuePair<DateTime, int>[]{
                 //new KeyValuePair<DateTime, int>(DateTime.Now, 100),
@@ -74,22 +74,22 @@ namespace PhysioApplication
                 //new KeyValuePair<DateTime, int>(DateTime.Now.AddMonths(2), 150),
                 //new KeyValuePair<DateTime, int>(DateTime.Now.AddMonths(3), 125),
                 //new KeyValuePair<DateTime, int>(DateTime.Now.AddMonths(4), 155) };
-            }
+            //}
         }
-        private void FilterComboBoxForCategorySelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            ComboBox categoryComboBox = (ComboBox)sender;
-            ComboBoxItem item = (ComboBoxItem)categoryComboBox.SelectedItem;
-            this.categoryID = Convert.ToInt32(item.Tag);
-            LoadLineChartData();
-        }
-        private void FilterComboBoxForTestsSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            ComboBox reportComboBox = (ComboBox)sender;
-            ComboBoxItem item = (ComboBoxItem)reportComboBox.SelectedItem;
-            this.reportID = Convert.ToInt32(item.Tag);
-            LoadLineChartData();
-        }
+        //private void FilterComboBoxForCategorySelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        //{
+        //    ComboBox categoryComboBox = (ComboBox)sender;
+        //    ComboBoxItem item = (ComboBoxItem)categoryComboBox.SelectedItem;
+        //    this.categoryID = Convert.ToInt32(item.Tag);
+        //    LoadLineChartData();
+        //}
+        //private void FilterComboBoxForTestsSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        //{
+        //    ComboBox reportComboBox = (ComboBox)sender;
+        //    ComboBoxItem item = (ComboBoxItem)reportComboBox.SelectedItem;
+        //    this.reportID = Convert.ToInt32(item.Tag);
+        //    LoadLineChartData();
+        //}
 
         private void LoadFirstSeries()
         {
